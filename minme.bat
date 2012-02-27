5d2b79efade595b1223a811f15c64c996c88174b
Web Advanced <paul.barton@webadvanced.com>
2012-02-27 15:08:59 -0800

    added nuget pkg

--- minme.bat
+++ minme.bat
@@ -1,5 +1,7 @@
 @echo off
-set PATH=lib\
-ajaxmin jmoney\jmoney.js -o jmoney\jmoney.min.js -clobber
-
+set PATH=\
+lib\ajaxmin jmoney\jmoney.js -o jmoney\jmoney.min.js -clobber
+copy jMoney\jmoney.min.js ".nuget\content\Scripts"
+copy jMoney\jmoney.js ".nuget\content\Scripts"
+.nuget\NuGet Pack .nuget\jMoney.js.nuspec -OutputDirectory .nuget\
 echo ----------------------------------------------
