db448575b3792d328ed91f628da9bbaf7192642b
Web Advanced <paul.barton@webadvanced.com>
2012-02-28 11:17:12 -0800

    cleaned up .nuget dir

--- minme.bat
+++ minme.bat
@@ -3,5 +3,6 @@ set PATH=\
 lib\ajaxmin jmoney\jmoney.js -o jmoney\jmoney.min.js -clobber
 copy jMoney\jmoney.min.js ".nuget\content\Scripts"
 copy jMoney\jmoney.js ".nuget\content\Scripts"
+del .nuget\*.nupkg
 .nuget\NuGet Pack .nuget\jMoney.js.nuspec -OutputDirectory .nuget\
 echo ----------------------------------------------
